19fe760a724e925ec7b82249be23a24cd89e4521
Maxim Demidov <demidovmax4@gmail.com>
2018-01-22 19:38:24 +0300

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,45 +1,88 @@
 import java.util.*;
 
 public class Parser {
-                public static void main(String[]args) {
-					HashMap<String, String> map = new HashMap<String, String>();
-				        Start test = new Start();
-					
-					map = test.doParse(args);
-					System.out.println(test.getValue(map, "test"));
-                }
+	public static void main(String[]args) {
+		HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+		ArrayList values = new ArrayList();
+		Start test = new Start();
+		
+		map = test.doParse(args);
+		test.printAll(map);
+	}
 }
 
 class Start {
-	public HashMap<String, String> doParse (String[]args) {
-		HashMap<String, String> map = new HashMap<String, String>();
-		int firstChar, endChar;
+	public HashMap<String, ArrayList<String>> doParse (String[]args) {
+		HashMap<String, ArrayList<String>> parameters = new HashMap<String, ArrayList<String>>();
+		HashMap<Integer,String> allParam = new HashMap<Integer,String>();
+		HashMap<String,String> allValues = new HashMap<String,String>();
+		String param = new String();
+		int firstChar = 0, endChar, paramCount = 0, valuesCount = 0;
+		
+		for(int i = 0; i < args.length; i++) {
+			if (args[i].startsWith("--") || args[i].startsWith("-") || args[i].startsWith("/")) {
+				paramCount++;
+				valuesCount = 0;
 				
-		for(int i=0; i < args.length; i+=2) {
-			firstChar = 0;
-			endChar = args[i].length();
-			
-			if(i == 0) {
-				for(int j = 0; j < 2; j++) {
-					if(args[i].charAt(j) == '/') {
-						firstChar = j + 1;
-						break;
-					}
-					else if (args[i].charAt(j) == '-')
-						firstChar ++;	
+				param = args[i].replace("-", "");
+				param = param.replace("/", "");
+				allParam.put(paramCount, param);
+				
+				if(param.indexOf(":") != -1 || param.indexOf("=") != -1) {
+					endChar = param.indexOf(":");
+					if(endChar == -1)
+						endChar = param.indexOf("=");
+					valuesCount++;
+					allValues.put(paramCount + "_" + valuesCount, param.substring(endChar + 1, param.length()));
+					param = param.substring(firstChar, endChar); 
 				}
-			
+				allParam.put(paramCount, param);
 			}
-			
-			map.put(args[i].substring(firstChar, endChar), args[i + 1]);
+			else {
+				valuesCount++;
+				allValues.put(paramCount + "_" + valuesCount, args[i]);
+			}
 		}
-		return map;
+		Iterator<Map.Entry<Integer, String>> iteratorParam = allParam.entrySet().iterator();
+		
+		while (iteratorParam.hasNext()) {
+			ArrayList values = new ArrayList();
+			Map.Entry<Integer, String> pairPar = iteratorParam.next();
+			Integer keyPar = pairPar.getKey();
+			String valuePar = pairPar.getValue();
+			Iterator<Map.Entry<String, String>> iteratorValues = allValues.entrySet().iterator();
+			
+			while (iteratorValues.hasNext()) {
+				Map.Entry<String, String> pairVal = iteratorValues.next();
+				String keyVal = pairVal.getKey();
+				String valueVal = pairVal.getValue();
+				if(keyVal.indexOf(keyPar + "_") != -1)
+					values.add(valueVal);
+			}
+			parameters.put(valuePar, values);
+		}
+		return parameters;
 	}
 	
-	public String getValue (HashMap<String, String> parameters, String paramName) {
+	public ArrayList<String> getValue (HashMap<String, ArrayList<String>> parameters, String paramName) {		
 		if(parameters.get(paramName) != null)
 			return parameters.get(paramName);
-		else 
-			return null; 
+		else
+			return null;
+	}
+	
+	public void printAll(HashMap<String, ArrayList<String>> parameters) {
+		Iterator<Map.Entry<String, ArrayList<String>>> iterator = parameters.entrySet().iterator();
+		
+		while (iterator.hasNext()) {
+			Map.Entry<String, ArrayList<String>> pair = iterator.next();
+			String param = pair.getKey();
+			ArrayList<String> values = pair.getValue();
+			System.out.println("Parameter: " + param);
+			System.out.println("Values:");
+			
+			for(String str : values)
+				System.out.println(str);
+		}
 	}
 }
